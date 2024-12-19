b74449be902eaf3fbcd3058334eb3fe6977b5b53
Abrikosochka <two.hundred.bucks.2000@gmail.com>
2024-12-19 19:49:27 +0300

    ai_generate

--- notebook-ai/src/components/Main/NoteRedactor/NoteRedactor.jsx
+++ notebook-ai/src/components/Main/NoteRedactor/NoteRedactor.jsx
@@ -1,6 +1,38 @@
 import "./noteRedactor.css"
+import { HfInference } from "@huggingface/inference"
+// import {useState} from "react";
 
 function NoteRedactor() {
+
+    // const [aiMessage, setAiMessage] = useState([])
+
+    async function handleAiGenerate(event) {
+        event.preventDefault()
+
+        let out = "";
+
+        const client = new HfInference(import.meta.env.VITE_AI_API_KEY)
+
+        const stream = client.chatCompletionStream({
+            model: "meta-llama/Llama-3.2-1B-Instruct",
+            messages: [
+                { role: "user", content: "Привет!!!!(Пиши на русском)" }
+            ],
+            temperature: 0.5,
+            max_tokens: 2048,
+            top_p: 0.7
+        });
+
+        for await (const chunk of stream) {
+            if (chunk.choices && chunk.choices.length > 0) {
+                const newContent = chunk.choices[0].delta.content;
+                out += newContent;
+            }
+        }
+
+        console.log(out);
+    }
+
     return (
         <div className="note-redactor">
             <div className="redactor">
@@ -20,7 +52,7 @@ function NoteRedactor() {
             <div className="ai-request">
                 <form>
                     <input type="text"/>
-                    <input type="submit" value=" "/>
+                    <input type="submit" value=" " onClick={(e)=>handleAiGenerate(e)}/>
                 </form>
             </div>
         </div>
